5790fd12b64a1c4cd93ce33b76650d73def4bf94
eldar23451 <liyev8024@gmail.com>
2024-05-17 22:05:21 +0400

    method faile is added

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//void Sum()
+//{1.parametrsiz ededlerin cemi
+//    Console.WriteLine("Please enter first number");
+//    int number=int.Parse(Console.ReadLine());
+//    Console.WriteLine("Please enter second number");
+//    int number2=int.Parse(Console.ReadLine());
+//    Console.WriteLine(number+number2);
+//}
+//Sum();
+//void Sum(int num1,int num2)
+//{
+//    Console.WriteLine(num1+ num2);-PARAMETRLI
+
+
+
+
+//}Sum(22, 55);
+
+
+
+}
+
+
+   
+
+
+}
+
+
+    
